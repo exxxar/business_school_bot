87e5f6e919e7be84508ed5b7b282b98427ed9c19
exxxar <exxxar@gmail.com>
2020-03-16 21:41:54 +0300

    first commit

--- routes/botman.php
+++ routes/botman.php
@@ -1,7 +1,31 @@
 <?php
 
-use App\Http\Controllers\BotController;
-
 $botman = resolve('botman');
 
-$botman->fallback(BotController::class . "@fallback");
+$botman->fallback(function ($bot){
+    $queryObject = json_decode($bot->getDriver()->getEvent());
+
+    $id = $bot->getUser()->getId();
+
+    $query = $queryObject->query;
+
+    $postdata = http_build_query(
+        array(
+            'chatId' => $id ,
+            'bot_name'=>env("MY_BOT_NAME"),
+            'query' => $query
+        )
+    );
+
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-Type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+
+    $context = stream_context_create($opts);
+
+    $result = file_get_contents('http://skidka-service.ru/api/v1/methods', false, $context);
+});
