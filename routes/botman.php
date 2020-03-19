de87dfffba9c86c166678076ee98100305be9901
exxxar <exxxar@gmail.com>
2020-03-19 22:36:53 +0300

    test

--- routes/botman.php
+++ routes/botman.php
@@ -18,7 +18,7 @@ $botman->fallback(function (\BotMan\BotMan\BotMan $bot) {
     $data = array(
         'user' => json_encode($bot->getUser()->getInfo()),
         'bot_url' => env("MY_BOT_NAME"),
-        //'message_id' => isset($bot->getMessage()->getPayload()["message_id"]) ? $bot->getMessage()->getPayload()["message_id"] : null,
+        'message_id' => isset($bot->getMessage()->getPayload()["message_id"]) ? $bot->getMessage()->getPayload()["message_id"] : null,
         'query' => $queryObject ?? ''
     );
 
