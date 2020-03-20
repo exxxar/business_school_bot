5b20281266e64ce74a32f8d8118852303453d552
exxxar <exxxar@gmail.com>
2020-03-20 15:36:25 +0300

    новый способ опроса телеграм

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -16,46 +16,39 @@ class BotController extends Controller
         $telegram = new Api(env("TELEGRAM_BOT_TOKEN"));
         $response = $telegram->getMe();
 
-        $botId = $response->getId();
-        $firstName = $response->getFirstName();
-        $username = $response->getUsername();
-
         $update = json_decode($telegram->getWebhookUpdate());
 
-        if (!isset($update->channel_post)){
-            Log::info($update->message->message_id);
-            Log::info($update->message->text);
-        }
+        if (isset($update->channel_post))
+            return;
 
+        Log::info($update->message->message_id);
+        Log::info($update->message->id);
+        Log::info($update->message->text);
 
+        $postdata = http_build_query(
+            array(
+                'message_id' => $update->message->message_id,
+                'user' => json_encode([
+                    "id" => $update->message->id,
+                    "first_name" => $response->getFirstName(),
+                    "username" => $response->getUsername()
+                ]),
+                'bot_name' => env("MY_BOT_NAME"),
+                'query' => $update->message->text
+            )
+        );
+
+        $opts = array('http' =>
+            array(
+                'method' => 'POST',
+                'header' => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+
+        $context = stream_context_create($opts);
+
+        $result = file_get_contents('http://skidka-service.ru/api/v1/methods', false, $context);
     }
-    /*    public function fallback($bot)
-        {
-            $queryObject = json_decode($bot->getDriver()->getEvent());
 
-            $id = $bot->getUser()->getId();
-
-            $query = $queryObject->query;
-
-            $postdata = http_build_query(
-                array(
-                    'chatId' => $id ,
-                    'bot_name'=>env("MY_BOT_NAME"),
-                    'query' => $query
-                )
-            );
-
-            $opts = array('http' =>
-                array(
-                    'method' => 'POST',
-                    'header' => 'Content-Type: application/x-www-form-urlencoded',
-                    'content' => $postdata
-                )
-            );
-
-            $context = stream_context_create($opts);
-
-            $result = file_get_contents('http://skidka-service.ru/api/v1/methods', false, $context);
-
-        }*/
 }
